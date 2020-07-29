0b683e6c79725f6cce98f7cd3afb22532ea7a6ca
safronman <safronmanbox@gmail.com>
2020-07-29 11:28:25 +0300

    3. adaptive menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { MenuComponent } from './menu/menu.component';
+import { MenuItemComponent } from './menu/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarouselComponent,
-    MenuComponent
+    MenuComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,10 @@
 import {Component, HostBinding} from '@angular/core';
 
+type MobMenuItemType = {
+    title: string
+    icon: string
+};
+
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
@@ -10,6 +15,15 @@ export class MenuComponent {
     isMenuOpen = false;
     menuOpen = '../../assets/icons/menu-open.svg';
     menuClose = '../../assets/icons/menu-close.svg';
+
+    mobItems: MobMenuItemType[] = [
+        {title: 'Главная', icon: 'home-icon.svg'},
+        {title: 'Биты', icon: 'beats-icon.svg'},
+        {title: 'Лицензии и цены', icon: 'license-price-icon.svg'},
+        {title: 'Товары', icon: 'goods-icon.svg'},
+        {title: 'Услуги', icon: 'services-icon.svg'},
+        {title: 'Контакты', icon: 'contacts-icon.svg'}
+    ];
 
     constructor() {
     }
